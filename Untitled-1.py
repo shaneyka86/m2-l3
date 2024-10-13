9d1dc8b5258e359ad649da518ce3c4298bf0f1ff
shaneyka86 <shaneyekefre@gmail.com>
2024-10-13 04:14:44 -1000

    Delete Untitled-1.py
    duplicate

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-#Task 2: Calculate the average grade and print it.
-
-grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
-grades.sort()
-total = sum(grades)
-average =total/len(grades)
-print(grades)
-print("The average grade is:", average)
